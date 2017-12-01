9ac62d6e4a1752396dc0b7dc04f2674a816ca3d7
LIXING <xingliself@gmail.com>
2017-12-01 13:25:28 +0800

    add a utility class

---
+++ CommandLineParser/src/com/commandlineparser/util/Util.java
@@ -0,0 +1,37 @@
+package com.commandlineparser.util;
+
+final class Util {
+	
+	/**
+	 * Remove the hyphens from the beginning of <code>str</code> and then return the new string.
+	 * 
+	 * @param str
+	 * @return
+	 */
+	static String stripLeadingHyphens(String str) {
+		if (null == str)
+			return null;
+		if (str.startsWith("--"))
+			return str.substring(2);
+		else if (str.startsWith("-"))
+			return str.substring(1);
+		
+		return str;
+	}
+	
+	/**
+	 * Remove the leading and trailing quotes from <code>str</code>
+	 * 
+	 * @param str
+	 * @return
+	 */
+	static String stripLeadingAndTrailingQuotes(String str) {
+		int length = str.length();
+		if (length > 1 && str.startsWith("\"") && str.endsWith("\"") 
+				&& str.substring(1, length - 1).indexOf('"') == -1) {
+			return str.substring(1, length - 1);
+		}
+		
+		return str;
+	}
+}
